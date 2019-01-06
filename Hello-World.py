d4c4260c74ccd68ad15ffb5aaa288f8bbe8360c1
Byen23 <46408750+Byen23@users.noreply.github.com>
2019-01-06 01:59:23 -0800

    Hello-World Program

---
+++ Hello-World.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan  6 01:54:53 2019
+
+@author: Byen23
+"""
+
+# My First Hello World program that I will be uploading to Github.
+
+print("Hello World\nThis is my very first program.")
+print("I will be uploading this program to Github.")
